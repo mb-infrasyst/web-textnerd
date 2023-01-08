b72f46ec90b47d70527c4a51795a8ceb9fec06b6
Melvin Batista <mbatista@infrasystpr.com>
2023-01-08 00:34:54 -0400

    first commit - all files

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+<h1>TextNerd.io</h1>
+  <AllCapsForm />
+
+  <small>Copyright 2022 <a href="https://www.infrasyst.com">InfraSyst Technology Solutions LLC</a></small><br>
+  <small>All rights reserved.</small>
+</template>
+
+<script>
+import AllCapsForm from "./components/TextTools.vue";
+export default {
+  name: "App",
+  components: { AllCapsForm },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
---
+++ src/components/TextTools.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="mainform">
+    <form>
+      <label>Enter text:</label><br />
+      <textarea type="text" rows="10" autofocus v-model="inputtext" />
+      <br />
+    </form>
+    <center>
+      <button class="actionbuttom" @click="clear()">Clear</button>
+      <button class="actionbuttom" @click="uppercase()">Uppercase</button>
+      <button class="actionbuttom" @click="lowercase()">Lowercase</button>
+      <button class="actionbuttom" @click="capcase()">CapCase</button>
+      <button class="actionbuttom" @click="alternatecase()">
+        AlternateCase
+      </button>
+      <button class="actionbuttom" @click="invertedcase()">InvertedCase</button>
+      <button class="actionbuttom" @click="sentencecase()">SentenceCase</button>
+    </center>
+  </div>
+
+  <hr />
+</template>
+
+<script>
+const headers_global = new Headers({
+  "Content-Type": "application/json",
+  "X-RapidAPI-Key": process.env.VUE_APP_RAPID_API_KEY,
+  "X-RapidAPI-Host": process.env.VUE_APP_RAPID_API_HOST,
+});
+export default {
+  data() {
+    return {
+      inputtext: "the force is good. this is the way.",
+    };
+  },
+  methods: {
+    // clear
+    clear() {
+      this.inputtext = "";
+    },
+
+    // connect to uppercase api
+    async uppercase() {
+      const requestOptions = {
+        method: "POST",
+        headers: headers_global,
+        body: JSON.stringify({ inputText: this.inputtext }),
+      };
+      const response = await fetch(
+        "https://textnerd-io.p.rapidapi.com/api/uppercase",
+        requestOptions
+      );
+      const data = await response.json();
+      console.log(data)
+      this.inputtext = data.uppercaseText;
+    },
+
+    // connect to lowercase api
+    async lowercase() {
+      const requestOptions = {
+        method: "POST",
+        headers: headers_global,
+        body: JSON.stringify({ inputText: this.inputtext }),
+      };
+      const response = await fetch(
+        "https://textnerd-io.p.rapidapi.com/api/lowercase",
+        requestOptions
+      );
+      const data = await response.json();
+      console.log(data)
+      this.inputtext = data.lowercaseText;
+    },
+
+    // connect to capcase api
+    async capcase() {
+      const requestOptions = {
+        method: "POST",
+        headers: headers_global,
+        body: JSON.stringify({ inputText: this.inputtext }),
+      };
+      const response = await fetch(
+        "https://textnerd-io.p.rapidapi.com/api/capcase",
+        requestOptions
+      );
+      const data = await response.json();
+      console.log(data)
+      this.inputtext = data.capcaseText;
+    },
+
+    // connect to alternatecase api
+    async alternatecase() {
+      const requestOptions = {
+        method: "POST",
+        headers: headers_global,
+        body: JSON.stringify({ inputText: this.inputtext }),
+      };
+      const response = await fetch(
+        "https://textnerd-io.p.rapidapi.com/api/alternatecase",
+        requestOptions
+      );
+      const data = await response.json();
+      console.log(data)
+      this.inputtext = data.alternatecaseText;
+    },
+
+    // connect to invertedcase api
+    async invertedcase() {
+      const requestOptions = {
+        method: "POST",
+        headers: headers_global,
+        body: JSON.stringify({ inputText: this.inputtext }),
+      };
+      const response = await fetch(
+        "https://textnerd-io.p.rapidapi.com/api/invertedcase",
+        requestOptions
+      );
+      const data = await response.json();
+      console.log(data)
+      this.inputtext = data.invertedcaseText;
+    },
+
+    // connect to sentencecase api
+    async sentencecase() {
+      const requestOptions = {
+        method: "POST",
+        headers: headers_global,
+        body: JSON.stringify({ inputText: this.inputtext }),
+      };
+      const response = await fetch(
+        "https://textnerd-io.p.rapidapi.com/api/sentencecase",
+        requestOptions
+      );
+      const data = await response.json();
+      console.log(data)
+      this.inputtext = data.sentencecaseText;
+    },
+  },
+};
+</script>
+
+<style>
+.mainform {
+  max-width: 60%;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  /* color: #aaa; */
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 1em;
+  /* text-transform: uppercase; */
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+textarea {
+  display: block;
+  resize: none;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  /* border: none; */
+  /* border-bottom: 1px solid #ddd; */
+  /* color: #555; */
+}
+.output {
+  max-width: 60%;
+  margin: 10px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+  font-weight: normal;
+}
+.actionbuttom {
+  font-size: 1.2em;
+  margin: 5px;
+}
+hr {
+  max-width: 60%;
+}
+</style>
