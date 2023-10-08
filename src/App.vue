572adbd3a07773c4924ac4d781ca8986640f717c
Melvin Batista <mbatista@infrasystpr.com>
2023-10-08 14:36:04 -0400

    version 1.0.1 cheking pipeline dist

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <!-- prettier-ignore -->
 <template>
-  <nav class="navbar is-link" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="https://www.textnerd.io">
-        <img
-          src="../public/img/textnerd-images/textnerd-logo-512x512-v1.png"
-          alt="Convert text case."
-        />
-      </a>
-    </div>
-    <!-- prettier-ignore -->
-    <div id="navbarBasicExample" class="navbar-menu">
+    <nav class="navbar is-link" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="https://www.textnerd.io">
+                <img
+                    src="../public/img/textnerd-images/textnerd-logo-512x512-v1.png"
+                    alt="Convert text case."
+                />
+            </a>
+        </div>
+        <!-- prettier-ignore -->
+        <div id="navbarBasicExample" class="navbar-menu">
       <div class="navbar-start">
         <a class="navbar-item" href="https://www.textnerd.io"> textnerd.io </a>
         <a class="navbar-item" href="https://rapidapi.com/melbatista76/api/textnerd-io" target="_blank"> API </a>
@@ -27,41 +27,44 @@
         </div>
       </div>
     </div>
-  </nav>
-  <AllCapsForm />
+    </nav>
+    <AllCapsForm />
 
-  <small
-    >Copyright 2023
-    <a href="https://www.infrasyst.com"
-      >InfraSyst Technology Solutions LLC</a
-    ></small
-  ><br />
-  <small>All rights reserved. version 1.0.0</small><br />
-  <small>POC application using VueJS, ExpressJS, GitHub Actions, RapidAPI, Heroku and Azure Static Web Apps.</small>
-  <p></p>
-  <p>&nbsp;</p>
+    <small
+        >Copyright 2023
+        <a href="https://www.infrasyst.com"
+            >InfraSyst Technology Solutions LLC</a
+        ></small
+    ><br />
+    <small>All rights reserved. version 1.0.1</small><br />
+    <small
+        >POC application using VueJS, ExpressJS, GitHub Actions, RapidAPI,
+        Heroku and Azure Static Web Apps.</small
+    >
+    <p></p>
+    <p>&nbsp;</p>
 </template>
 
 <script>
 import AllCapsForm from "./components/TextTools.vue";
 export default {
-  name: "App",
-  components: { AllCapsForm },
+    name: "App",
+    components: { AllCapsForm },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  /* margin-top: 0px; */
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /* margin-top: 0px; */
 }
 body {
-  margin: 0;
-  background: #eee;
-  height: 100%;
+    margin: 0;
+    background: #eee;
+    height: 100%;
 }
 </style>
